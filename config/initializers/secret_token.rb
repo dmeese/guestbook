dd733eb87a9c8cb356408995b256b2f14a090c19
David Meese <github@myrddins.com>
2012-10-29 15:24:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = 'a2228db1511551087b5b3381e0d3b4634068686886913ac3c168a02d8ad11aba095fc51b13109187e329aea8cf7d755d3fba5e7d7939f1c94981bd5e065ca684'
